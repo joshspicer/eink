67bcd1981c25ca3a61e266e74d9ffc6de9f1a945
Josh Spicer <hello@joshspicer.com>
2023-03-27 20:05:24 -0700

    params

--- barebones_inkplate/params.h
+++
@@ -1,6 +0,0 @@
-// WiFi network SSID (network name) and password
-const char *wifi_network  = "";
-const char *wifi_password = "";
-
-// Time to be in deep sleep. This is in seconds.
-uint sleep_time = 30;
