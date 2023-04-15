86d56ae985d7d897e75563e87f69dc6d29fbf430
Josh Spicer <josh@joshspicer.com>
2023-04-15 02:45:39 +0000

    instructions

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 - https://github.com/SolderedElectronics/Inkplate-Arduino-library
+
+```bash
+cd build/
+docker build . -t eink-today
+
+docker run --rm -v /workspaces/eink/:/output  eink-today
+```
--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-TEX_SOURCE_DIR="${1:-/workspaces/eink/examples/newspaper}"
+if [ ! -d /output ]; then
+    echo "Output directory does not exist! Bind mount a directory to /output"
+    exit 1
+fi
 
-mkdir -p /tmp/build-newspaper
-cp -r $TEX_SOURCE_DIR tmp/build-newspaper
+# Do whatever replacements in 'template.tex' to add content
 
-pdflatex -interaction=nonstopmode /tmp/template.tex -output-directory /tmp/compiled.pdf
-docker run --rm -v /workspaces/eink/examples/newspaper/:/imgs dpokidov/imagemagick /imgs/newspaperExample.pdf  -depth 1 -monochrome -resize 1200x  BMP3:/imgs/output.bmp
+pdflatex -interaction=nonstopmode /build/template.tex
+convert /build/template.pdf  -depth 1 -monochrome -resize 1200x  BMP3:/output/output.bmp
